2779962af04051bcbc14c3c69aa7c160cb8318a5
2295790516@qq.com <2295790516@qq.com>
2017-06-05 11:16:31 +0800

    更改导航

--- js/developer.js
+++ js/developer.js
@@ -12,25 +12,25 @@ $(function () {
         var cur=0;
         $(window).scroll(function (event) {
             var windPos=$(window).scrollTop();
-            console.log(windPos);
             //500---ssp
             if(cur!=0 && (windPos>500 && windPos<520)){
                 cur=0;
             }else if(cur!=1 && (windPos>1000 && windPos<1020)){
                 cur=1;
+            }else if(cur!=2 && (windPos>2255 && windPos<2275)){
+                cur=2;
             }else if(windPos==$(document).height()-$(window).height()){
-                cur=1;
+                cur=2;
             }else if(windPos==0){
                 cur=0;
             }
-            var navArr=[535,1210];
+            var navArr=[535,1210,2236];
             for(var i=0;i<navArr.length;i++){
                 if(navArr[i]==windPos){
                     cur=i;
                     break;
                 }
             }
-
             $(".nav-box>a").removeClass("current");
             $(".nav-box>a").eq(cur).addClass("current");
         });
